54457aff5502795c24cc3b0668693693cc7d4420
sangam5756 <sangammunde3@gmail.com>
2024-06-30 13:31:20 +0530

    resume link add

--- src/pages/Home/intro.jsx
+++ src/pages/Home/intro.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 import { useRef } from "react";
 import About from "./About";
-import {Intro} from "../../../resources/intro.js"
+import { Intro } from "../../../resources/intro.js"
 function intro() {
   const { loading, portfoliodata } = useSelector((state) => state.root);
   // const { intro } = portfoliodata;
@@ -10,7 +10,7 @@ function intro() {
   // console.log("Intro",Intro)
   const aboutRef = useRef(null);
 
-const handleGetStartedClick = () => {
+  const handleGetStartedClick = () => {
     if (aboutRef.current) {
       aboutRef.current.scrollIntoView({ behavior: "smooth" });
     }
@@ -19,11 +19,11 @@ const handleGetStartedClick = () => {
     <>
       <div className="h-[80vh] bg-primary flex flex-col items-start justify-center gap-8 py-10">
         <h1 className="text-white font-semibold ">
-        <img src="https://readme-typing-svg.herokuapp.com/?font=Righteous&size=35&vCenter=true&width=500&height=70&duration=3000&lines=Hi+There!+👋;" />
-        
+          <img src="https://readme-typing-svg.herokuapp.com/?font=Righteous&size=35&vCenter=true&width=500&height=70&duration=3000&lines=Hi+There!+👋;" />
+
         </h1>
         <h1 className=" text-7xl sm:text-3xl text-secondary font-semibold">
-        <img src="https://readme-typing-svg.herokuapp.com/?font=Righteous&size=35&vCenter=true&width=500&height=70&duration=3000&lines=I'm;" />
+          <img src="https://readme-typing-svg.herokuapp.com/?font=Righteous&size=35&vCenter=true&width=500&height=70&duration=3000&lines=I'm;" />
           {firstName || " "} {lastName || " "}
         </h1>
         <h1 className="text-7xl sm:text-3xl text-white font-semibold">
@@ -31,10 +31,16 @@ const handleGetStartedClick = () => {
         </h1>
         <p className="text-white w-2/3">{description || " "}
         </p>
-        <button className=" border-2 border-tertiery px-10 py-3 text-tertiery rounded"
-         onClick={handleGetStartedClick}>
-          Get Started
-        </button>
+        <div className="flex gap-3 justify-between">
+          <button className=" border-2 border-tertiery px-10 py-3 text-tertiery rounded"
+            onClick={handleGetStartedClick}>
+            Get Started
+          </button>
+          <button className=" border-2 border-tertiery px-10 py-3 text-tertiery rounded"
+            ><a href="/resume.pdf" target="_blank">Get Resume</a>
+            
+          </button>
+        </div>
 
       </div>
       <About aboutRef={aboutRef} />
