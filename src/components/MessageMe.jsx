09a53283a250d8bf37b57719699e110083db9380
sangam5756 <sangammunde3@gmail.com>
2024-07-04 16:22:13 +0530

    textbox

--- src/components/MessageMe.jsx
+++ src/components/MessageMe.jsx
@@ -31,12 +31,12 @@ const MessageMe = () => {
 
         <form ref={form} className='text-white h-40 sm:w-80  sm:ml-80 ml-24 mb-24' onSubmit={sendEmail}>
             <label>Name</label>
-            <input type="text" name="from_name" required className='  focus:bg-white curs text-black bg-primary rounded outline-none p-2' />
+            <input type="text" name="from_name" required className='  focus:bg-white curs focus:text-black text-white bg-primary rounded outline-none p-2' />
             <label>Email</label>
-            <input type="email" name="from_email" required className=' focus:bg-white curs text-black bg-primary rounded outline-none p-2' />
+            <input type="email" name="from_email" required className=' focus:bg-white curs focus:text-black text-white bg-primary rounded outline-none p-2' />
             <label>Message</label>
             <textarea name="message" className=' focus:bg-white curs text-black bg-primary rounded outline-none p-2' maxLength={1000} />
-            <input type="submit" required className='bg-tertiery text-black hover:text-white hover:bg-primary px-2 py-1  cursor-pointer ' value="Send" />
+            <input type="submit" required className='bg-tertiery text-black hover:text-white  focus:text-black text-white hover:bg-primary px-2 py-1  cursor-pointer ' value="Send" />
 
         </form>
 
